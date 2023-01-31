4776a743770581eb9bf583ef3eda7695a5cd0004
LincKim <lincolnkimutai@gmail.com>
2023-01-31 10:00:45 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,29 @@
 // create your App component here
+import React, {useEffect, useState} from "react";
+
+function App (){
+    const [image, setImage] = useState('')
+    const imagesUrl = "https://dog.ceo/api/breeds/image/random"
+
+    useEffect(()=>{
+        fetch(imagesUrl)
+        .then(r => r.json())
+        .then(data=>{
+            console.log(data.message)
+            setImage(data.message)
+        })
+    },[])
+    if(image===""){
+        return <p>Loading...</p>
+    }
+
+    return(
+        <div>
+            <img src={image} alt="A Random Dog" />
+        </div>
+    )
+
+
+}
+
+export default App;
